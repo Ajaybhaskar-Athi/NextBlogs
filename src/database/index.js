d71c1b84456f6630ef3885349bc159f92b8162c3
Ajaybhaskar-Athi <ajaybhaskar.athi@gmail.com>
2024-11-01 20:52:37 +0530

    NextJS-Blog

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB=async ()=>{
    try{
-    await mongoose.connect(process.env.MONGODB_URL,{
+    await mongoose.connect(process.env.MONGODB_URL ,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
   });
